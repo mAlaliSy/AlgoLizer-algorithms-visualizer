08af1f3a96524c7409433cb00b2af2d4bcdd8aef
Mohammad AlAli <m.alali.sy@gmail.com>
2019-09-15 07:40:57 +0300

    feat: Initial implementation of A* algorithm

---
+++ app/src/main/java/com/malalisy/algolizer/domain/shortestpath/algorithmsimp/AStarAlgorithmRunner.kt
@@ -0,0 +1,55 @@
+package com.malalisy.algolizer.domain.shortestpath.algorithmsimp
+
+import com.malalisy.algolizer.domain.shortestpath.ShortestPathAlgorithmRunner
+import com.malalisy.algolizer.domain.shortestpath.ShortestPathNode
+import com.malalisy.algolizer.domain.shortestpath.TileType
+import java.util.*
+import kotlin.math.abs
+
+class AStarAlgorithmRunner(grid: Array<Array<TileType>>) : ShortestPathAlgorithmRunner(grid) {
+
+    override fun run(source: Pair<Int, Int>, destination: Pair<Int, Int>) {
+        val costs = Array(grid.size) {
+            Array(grid[it].size) { pos -> Int.MAX_VALUE }
+        }
+        costs[source.first][source.second] = 0
+        val queue = PriorityQueue<ShortestPathNode>(20) { a, b ->
+            a.cost - b.cost
+        }
+        queue.add(ShortestPathNode(source, 0, null))
+        while (queue.isNotEmpty()) {
+            val node = queue.poll()
+            if (node.position == destination) {
+                destinationReached = true
+                findSolution(node)
+                break
+            }
+
+            for (neighbor in findNeighbors(node.position)) {
+                if (grid[neighbor.first][neighbor.second] == TileType.Block) continue
+                val ncost = costs[node.position.first][node.position.second] + 1
+                if (ncost < costs[neighbor.first][neighbor.second]) {
+                    costs[neighbor.first][neighbor.second] =
+                        costs[node.position.first][node.position.second] + 1
+                    queue.add(
+                        ShortestPathNode(
+                            neighbor,
+                            ncost + heuristic(neighbor, destination),
+                            node
+                        )
+                    )
+
+                    orderedVisitedCells.add(neighbor)
+                }
+            }
+
+
+        }
+
+    }
+
+    private fun heuristic(point: Pair<Int, Int>, destination: Pair<Int, Int>): Int {
+        return abs(point.first - destination.first) + abs(point.second - destination.second)
+    }
+
+}
