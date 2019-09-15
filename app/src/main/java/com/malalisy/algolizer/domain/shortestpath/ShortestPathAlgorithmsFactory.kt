30b0c9e19e2d4e51f7cf10a372adba1e7ee4ffa8
Mohammad AlAli <m.alali.sy@gmail.com>
2019-09-15 22:35:01 +0300

    feat: Create a shortest path algorithm factory

---
+++ app/src/main/java/com/malalisy/algolizer/domain/shortestpath/ShortestPathAlgorithmsFactory.kt
@@ -0,0 +1,34 @@
+package com.malalisy.algolizer.domain.shortestpath
+
+import androidx.annotation.IntDef
+import com.malalisy.algolizer.domain.shortestpath.algorithmsimp.AStarAlgorithmRunner
+import com.malalisy.algolizer.domain.shortestpath.algorithmsimp.BfsAlgorithmRunner
+import com.malalisy.algolizer.domain.shortestpath.algorithmsimp.DijkstraAlgorithmRunner
+import com.malalisy.algolizer.domain.shortestpath.algorithmsimp.GreedyBestFirstAlgorithmRunner
+import java.lang.annotation.RetentionPolicy
+
+
+object ShortestPathAlgorithmsFactory {
+
+    const val DIJKSTRA_ALGORITHM = 0
+    const val AStar_ALGORITHM = 1
+    const val BFS_ALGORITHM = 2
+    const val GREEDY_BEST_FIRST_ALGORITHM = 3
+
+    @IntDef(DIJKSTRA_ALGORITHM, AStar_ALGORITHM, BFS_ALGORITHM, GREEDY_BEST_FIRST_ALGORITHM)
+    @Retention(AnnotationRetention.SOURCE)
+    annotation class ShortestPathAlgorithm
+
+
+    fun getAlgorithmRunner(@ShortestPathAlgorithm algorithm: Int, grid: Array<Array<TileType>>)
+            : ShortestPathAlgorithmRunner {
+        return when (algorithm) {
+            DIJKSTRA_ALGORITHM -> DijkstraAlgorithmRunner(grid)
+            AStar_ALGORITHM -> AStarAlgorithmRunner(grid)
+            BFS_ALGORITHM -> BfsAlgorithmRunner(grid)
+            GREEDY_BEST_FIRST_ALGORITHM -> GreedyBestFirstAlgorithmRunner(grid)
+
+            else -> throw IllegalArgumentException("Unsupported algorithm")
+        }
+    }
+}
