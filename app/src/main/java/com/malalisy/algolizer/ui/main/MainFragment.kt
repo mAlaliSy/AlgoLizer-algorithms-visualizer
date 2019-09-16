567ef4257abed1b9dd4461b5963065b2cb6ee6b4
Mohammad AlAli <m.alali.sy@gmail.com>
2019-09-16 11:58:53 +0300

    feat: Add main fragment

---
+++ app/src/main/java/com/malalisy/algolizer/ui/main/MainFragment.kt
@@ -0,0 +1,34 @@
+package com.malalisy.algolizer.ui.main
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+
+import com.malalisy.algolizer.R
+import kotlinx.android.synthetic.main.fragment_main.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnShortestPathAlgorithms.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_mainFragment_to_shortestPathAlgorimFragment))
+    }
+
+
+}
