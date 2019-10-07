60b0aaa8e2d34068e95590fb0b2aa613c43f9375
mohammad <m.alali.sy@gmail.com>
2019-10-07 23:23:33 +0300

    feat: add vertices to GraphView when touching the screen

--- app/src/main/java/com/malalisy/algolizer/ui/views/graphview/GraphView.kt
+++ app/src/main/java/com/malalisy/algolizer/ui/views/graphview/GraphView.kt
@@ -1,10 +1,15 @@
 package com.malalisy.algolizer.ui.views.graphview
 
+import android.animation.*
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.drawable.ColorDrawable
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
+import android.view.animation.AccelerateInterpolator
 
 class GraphView @JvmOverloads constructor(
     context: Context?,
@@ -12,9 +17,24 @@ class GraphView @JvmOverloads constructor(
     defstyle: Int = 0
 ) : View(context, attrs, defstyle) {
 
+    companion object {
+        const val DEFAULT_VERTEX_INNER_RADIUS = 50f
+        const val DEFAULT_VERTEX_OUTER_RADIUS = 60f
+        val DEFAULT_VERTEX_INNER_COLOR = Color.parseColor("#FF9800")
+        val DEFAULT_VERTEX_OUTER_COLOR = Color.parseColor("#FF673AB7")
+        val DEFAULT_TRANSITION_COLOR = Color.parseColor("#FFF04C7F")
 
-    private val vertices: List<VertexViewItem> = listOf()
-    private val verticesPaint:Paint
+
+    }
+
+    private val vertices: MutableList<VertexViewItem> = mutableListOf()
+    private val verticesPaint: Paint
+    private var vertexInnerRadius = DEFAULT_VERTEX_INNER_RADIUS
+    private var vertexOuterRadius = DEFAULT_VERTEX_OUTER_RADIUS
+
+    private var transitionColor = DEFAULT_TRANSITION_COLOR
+    private var vertexInnerColor = DEFAULT_VERTEX_INNER_COLOR
+    private var vertexOuterColor = DEFAULT_VERTEX_OUTER_COLOR
 
     init {
         verticesPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
@@ -33,7 +53,7 @@ class GraphView @JvmOverloads constructor(
 
     private fun drawVertices(canvas: Canvas?) {
         canvas?.let {
-            for (vertex in vertices){
+            for (vertex in vertices) {
                 verticesPaint.color = vertex.outerColor
                 it.drawCircle(vertex.x, vertex.y, vertex.outerRadius, verticesPaint)
                 verticesPaint.color = vertex.innerColor
@@ -42,14 +62,84 @@ class GraphView @JvmOverloads constructor(
         }
     }
 
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                addVertexItem(event.x, event.y)
+            }
+
+
+        }
+
+
+
+
+        return true
+    }
+
+    private fun addVertexItem(x: Float, y: Float) {
+        val vertexViewItem = VertexViewItem(
+            vertices.size, x, y,
+            0f, 0f, 0, 0
+        )
+        vertices.add(vertexViewItem)
+
+        val innerRadiusProperty =
+            PropertyValuesHolder.ofFloat("innerRadius", 25f, vertexInnerRadius)
+        val outerRadiusProperty =
+            PropertyValuesHolder.ofFloat("outerRadius", 0f, vertexOuterRadius)
+        val innerColorProperty =
+            PropertyValuesHolder.ofObject(
+                "innerColor",
+                ArgbEvaluator(),
+                transitionColor,
+                vertexInnerColor
+            )
+        val outerColorProperty =
+            PropertyValuesHolder.ofObject(
+                "outerColor",
+                ArgbEvaluator(),
+                transitionColor,
+                vertexOuterColor
+            )
+
+        ValueAnimator().apply {
+            setValues(
+                innerRadiusProperty,
+                outerRadiusProperty,
+                innerColorProperty,
+                outerColorProperty
+            )
+            duration = 300
+            addUpdateListener {
+                val innerRadius = it.getAnimatedValue("innerRadius") as Float
+                val outerRadius = it.getAnimatedValue("outerRadius") as Float
+                val innerColor = it.getAnimatedValue("innerColor") as Int
+                val outerColor = it.getAnimatedValue("outerColor") as Int
+                vertexViewItem.run {
+                    this.innerRadius = innerRadius
+                    this.outerRadius = outerRadius
+                    this.innerColor = innerColor
+                    this.outerColor = outerColor
+                }
+
+                invalidate()
+            }
+            interpolator = AccelerateInterpolator()
+        }.start()
+
+
+    }
+
 
     private data class VertexViewItem(
         val number: Int,
         val x: Float,
         val y: Float,
-        val innerRadius: Float,
-        val outerRadius: Float,
-        val innerColor: Int,
-        val outerColor: Int
+        var innerRadius: Float,
+        var outerRadius: Float,
+        var innerColor: Int,
+        var outerColor: Int
     )
 }
