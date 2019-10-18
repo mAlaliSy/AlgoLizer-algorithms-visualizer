160bb615cddaf02aed8ff430ce42f2053ce32907
mohammad <m.alali.sy@gmail.com>
2019-10-18 21:16:38 +0300

    feat: add the implementation of Prim's algorithm in PrimAlgorithmRunner

---
+++ app/src/main/java/com/malalisy/algolizer/domain/mst/PrimAlgorithmRunner.kt
@@ -0,0 +1,43 @@
+package com.malalisy.algolizer.domain.mst
+
+import java.util.*
+
+class PrimAlgorithmRunner(adjacencyMatrix: Array<Array<Int>>) :
+    MSTAlgorithmRunner(adjacencyMatrix) {
+
+    lateinit var visited: Array<Boolean>
+
+    override fun setup(adjacencyMatrix: Array<Array<Int>>) {
+        super.setup(adjacencyMatrix)
+        visited = Array(adjacencyMatrix.size) { false }
+    }
+
+    override fun run() {
+        val queue = PriorityQueue<Vertex>()
+
+        visited[0] = true
+        queue.add(Vertex(0, 0, -1))
+
+        while (queue.isNotEmpty()) {
+            val node = queue.poll()
+            if (visited[node.index]) continue
+            // Add the edge from parent to the node to the MST
+            if (node.parent != -1) {
+                mst.add(node.parent to node.index)
+                mstCost += adjacencyMatrix[node.parent][node.index]
+            }
+            visited[node.index] = true
+            for (i in adjacencyMatrix.indices) {
+                if (adjacencyMatrix[node.index][i] != 0) {
+                    queue.add(Vertex(i, adjacencyMatrix[node.index][i], node.index))
+                }
+            }
+        }
+
+    }
+
+
+    data class Vertex(val index: Int, var cost: Int, var parent: Int) : Comparable<Vertex> {
+        override fun compareTo(other: Vertex) = cost.compareTo(other.cost)
+    }
+}
