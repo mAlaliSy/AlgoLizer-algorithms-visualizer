09b53229894d994ea344b8211b7302be25ff4bc9
mohammad <m.alali.sy@gmail.com>
2019-10-18 18:58:51 +0300

    feat: add setup method to MSTAlgorithmRunner class

--- app/src/main/java/com/malalisy/algolizer/domain/mst/MSTAlgorithmRunner.kt
+++ app/src/main/java/com/malalisy/algolizer/domain/mst/MSTAlgorithmRunner.kt
@@ -3,16 +3,26 @@ package com.malalisy.algolizer.domain.mst
 /**
  * an abstract class for Minimum Cost Spanning Tree (MST) algorithms
  */
-abstract class MSTAlgorithmRunner {
+abstract class MSTAlgorithmRunner(adjacencyList: List<List<Pair<Int, Int>>>) {
 
+    init {
+        setup(adjacencyList)
+    }
 
     // A list that contains the edges of MST
-    var mst: List<Pair<Int, Int>>? = null
+    lateinit var mst: List<Pair<Int, Int>>
 
     // The total cost of MST
     var mstCost: Int = -1
 
     // The graph represented in an adjacency list
-    var adjacencyList:List<List<Pair<Int, Int>>>? = null
+    private lateinit var adjacencyList: List<List<Pair<Int, Int>>>
+
+
+    public open fun setup(adjacencyList: List<List<Pair<Int, Int>>>) {
+        this.adjacencyList = adjacencyList
+        mst = mutableListOf()
+        mstCost = -1
+    }
 
 }
