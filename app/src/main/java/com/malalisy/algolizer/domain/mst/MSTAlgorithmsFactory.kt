5026ce7db6dc0cff36e95467b73cbb12f1b50834
mohammad <m.alali.sy@gmail.com>
2019-10-19 20:12:20 +0300

    feat: add MSTAlgorithmsFactory

---
+++ app/src/main/java/com/malalisy/algolizer/domain/mst/MSTAlgorithmsFactory.kt
@@ -0,0 +1,24 @@
+package com.malalisy.algolizer.domain.mst
+
+import androidx.annotation.IntDef
+import java.lang.IllegalArgumentException
+
+object MSTAlgorithmsFactory {
+    const val KRUSKAL_ALGORITHM = 0
+    const val PRIMS_ALGORITHM = 1
+
+    @IntDef(KRUSKAL_ALGORITHM, PRIMS_ALGORITHM)
+    @Retention(AnnotationRetention.SOURCE)
+    annotation class MSTAlgorithms
+
+    public fun getAlgorithm(
+        @MSTAlgorithms algorithm: Int,
+        adjacencyMatrix: Array<Array<Int>>
+    ): MSTAlgorithmRunner {
+        return when (algorithm) {
+            KRUSKAL_ALGORITHM -> KruskalAlgorithmRunner(adjacencyMatrix)
+            PRIMS_ALGORITHM -> PrimAlgorithmRunner(adjacencyMatrix)
+            else -> throw IllegalArgumentException()
+        }
+    }
+}
