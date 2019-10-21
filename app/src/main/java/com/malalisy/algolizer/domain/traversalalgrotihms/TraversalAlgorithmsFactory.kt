9588760cfaa4a00c3bbc51979b598c00a02f527b
mohammad <m.alali.sy@gmail.com>
2019-10-21 20:35:55 +0300

    feat: add TraversalAlgorithmsFactory

--- app/src/main/java/com/malalisy/algolizer/domain/traversalalgrotihms/TraversalAlgorithmsFactory.kt
+++ app/src/main/java/com/malalisy/algolizer/domain/traversalalgrotihms/TraversalAlgorithmsFactory.kt
@@ -1,15 +1,27 @@
 package com.malalisy.algolizer.domain.traversalalgrotihms
 
 import androidx.annotation.IntDef
+import java.lang.IllegalArgumentException
 
 class TraversalAlgorithmsFactory {
     companion object {
 
-        public const val BFS = 0
-        public const val DFS = 1
+        const val BFS = 0
+        const val DFS = 1
 
         @IntDef(BFS, DFS)
         @Retention(AnnotationRetention.SOURCE)
         annotation class TraversalAlgorithms
+
+        fun getTraversalAlgorithm(
+            @TraversalAlgorithms algorithm: Int,
+            adjacencyMatrix: Array<Array<Int>>
+        ): TraversalAlgorithmRunner {
+            return when (algorithm) {
+                BFS -> BFSAlgorithmRunner(adjacencyMatrix)
+                DFS -> DFSAlgorithmRunner(adjacencyMatrix)
+                else -> throw IllegalArgumentException()
+            }
+        }
     }
 }
